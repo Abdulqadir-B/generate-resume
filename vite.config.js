d68623803efe027be0ed241caa1cfa091609d493
Abdulqadir <aqbearing@gmail.com>
2026-09-21 23:50:09 +0530

    style: enhance layout and styling for Resume components and update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+});
